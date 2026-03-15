436712accedfc765ec6febe9a72b027621ffbc83
newtime595 <newtime595@gmail.com>
2026-03-15 17:23:23 +0900

    260315 5:23 홍성인 admin 수정

--- Oulim/admin/js/member-manage/mem-list.js
+++ Oulim/admin/js/member-manage/mem-list.js
@@ -4,10 +4,10 @@
 //     location.href = "/Oulim/admin/html/member-manage/mem-edit.html";
 // });
 
-const editMember = document.querySelectorAll(".memedit");
+const editMember = document.querySelectorAll(".memdetail");
 
 editMember.forEach((btn) => {
   btn.addEventListener("click", function () {
-    location.href = "/Oulim/admin/html/member-manage/mem-edit.html";
+    location.href = "/Oulim/admin/html/member-manage/mem-detail.html";
   });
 });
